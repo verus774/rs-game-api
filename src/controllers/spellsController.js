1965a07dc5a3279c86272818a57f72aeea936b20
Sergey Ladutko <verus774@mail.ru>
2018-06-26 19:51:57 +0300

    fix: images URL

--- src/controllers/spellsController.js
+++ src/controllers/spellsController.js
@@ -2,9 +2,14 @@ const Spell = require('../models/Spell');
 
 exports.list = async (req, res) => {
     try {
-        const spells = await Spell.getAll();
+        let spells = await Spell.getAll();
+        spells = spells.map((spell) => {
+            spell.img = req.hostname + '/' + spell.img;
+            return spell;
+        });
         return res.json(spells);
     } catch (e) {
+        console.error(e);
         return res.status(500).json({
             status: 'error',
         });
--- src/models/Spell.js
+++ src/models/Spell.js
@@ -1,9 +1,5 @@
 const spells = require('../data/spells');
-const os = require('os');
 
 exports.getAll = async () => {
-    return Promise.resolve(spells.map((spell) => {
-        spell.img = os.hostname() + '/' + spell.img;
-        return spell;
-    }));
+    return Promise.resolve(spells);
 };
